140a83e84d4cd0cd96d3d974c06eaf1d1469ee4b
linh.bui <linh.bui@secomm.vn>
2026-08-24 11:12:17 +0700

    [SLP-29] Remove extension Magezon Pagebuilder

--- app/code/Magezon/Core/Framework/Controller/Result/Json.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Magezon\Core\Framework\Controller\Result;
-
-class Json extends \Magento\Framework\Controller\Result\Json
-{
-    public function getJson() {
-        return $this->json;
-    }
-}
